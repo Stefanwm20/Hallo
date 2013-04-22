fe77ca985d97a22ac772cb4dfce9e1fedb97028e
Stefan <stefantecnology84@hotmail.com>
2013-04-22 14:55:56 +0200

    Tamagotchi

---
+++ prp1_ss13/src/wagner_wendt__aufgabenblatt3/Tamagotchi.java
@@ -0,0 +1,28 @@
+package wagner_wendt__aufgabenblatt3;
+
+public class Tamagotchi {
+
+	int hunger;
+	int boredness;
+	int tiredness;
+
+	public void setHunger(int i) {
+		hunger = i;
+
+	}
+
+	public void setTiredness(int i) {
+		tiredness = i;
+
+	}
+
+	public void setBoredness(int i) {
+		boredness = i;
+	}
+
+	public int getHunger() {
+		return hunger;
+		return 0;
+	}
+
+}
